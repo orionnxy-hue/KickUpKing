4a6edfd6c61e2de3b70917bd8da26d60e238bad1
Orion <orion@kickupking.com>
2026-03-06 18:43:48 +0100

    feat: use environment variables for Supabase connection

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// These are your keys to talk to the "Cloud Whiteboard"
-const supabaseUrl = 'https://vjyotivssodzctofmopb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqeW90aXZzc29kemN0b2Ztb3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjE0NDAsImV4cCI6MjA4MjU5NzQ0MH0.ZDEAD2DLfWirzx0XMEUjHJWeo05SlfvOgub4gMbwhfI';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
